eeae65bf3f0c3471a983f25b82ef6356dc911be9
Naveen1825 <naveenkanthan12@gmail.com>
2024-10-15 00:07:12 +0530

    2530 solution

---
+++ Arrays/2530 Maximal Score After Applying K Operations.py
@@ -0,0 +1,49 @@
+"""
+You are given a 0-indexed integer array nums and an integer k. You have a starting score of 0.
+
+In one operation:
+
+choose an index i such that 0 <= i < nums.length,
+increase your score by nums[i], and
+replace nums[i] with ceil(nums[i] / 3).
+Return the maximum possible score you can attain after applying exactly k operations.
+
+The ceiling function ceil(val) is the least integer greater than or equal to val.
+
+ 
+
+Example 1:
+
+Input: nums = [10,10,10,10,10], k = 5
+Output: 50
+Explanation: Apply the operation to each array element exactly once. The final score is 10 + 10 + 10 + 10 + 10 = 50.
+Example 2:
+
+Input: nums = [1,10,3,3,3], k = 3
+Output: 17
+Explanation: You can do the following operations:
+Operation 1: Select i = 1, so nums becomes [1,4,3,3,3]. Your score increases by 10.
+Operation 2: Select i = 1, so nums becomes [1,2,3,3,3]. Your score increases by 4.
+Operation 3: Select i = 2, so nums becomes [1,1,1,3,3]. Your score increases by 3.
+The final score is 10 + 4 + 3 = 17.
+"""
+
+
+import heapq, math
+class Solution(object):
+    def maxKelements(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        max_heap = [-num for num in nums]
+        heapq.heapify(max_heap)
+        
+        score = 0
+        for _ in range(k):
+            n = -heapq.heappop(max_heap)
+            score += n
+            heapq.heappush(max_heap, -(n // 3 + (1 if n % 3 else 0)))
+        return score
+        
